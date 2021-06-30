1b6316db5060779e453a1fdcc385dd36842c4322
abbas sahin <47714325+shnab@users.noreply.github.com>
2021-06-30 19:39:58 +0200

    Api

---
+++ src/test/java/com/techproed/day01/GetRequest01.java
@@ -0,0 +1,53 @@
+package com.techproed.day01;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+public class GetRequest01 {
+
+    /*
+    GetRequest01:
+    https://restful-booker.herokuapp.com/booking/3 adresine bir request gonderildiginde donecek cevap(response) icin
+    HTTP status kodunun 200
+    Content Type’in Json
+    Ve Status Line’in HTTP/1.1 200 OK
+    Oldugunu test edin.
+     */
+
+    @Test
+    public void test01(){
+        //1.url belirleme
+        String url = "https://restful-booker.herokuapp.com/booking/3";
+
+        //2-expected result
+        //
+        //3-request gonder
+        Response response=
+                given().
+                accept("application/json").
+                when().
+                get(url);
+        //api ile print etmek icin
+        response.prettyPrint();
+
+        //4-response al(actual result)
+        //body testi yapmadigimiz icin actual result almiyoruz
+
+        //5-assertion islemini yapma
+        response.
+                then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                statusLine("HTTP/1.1 200 OK");
+
+        System.out.println("StatusCode: " +  response.getStatusCode());
+        System.out.println("statusLine : " + response.statusLine());
+        System.out.println("contentType : " + response.contentType());
+        System.out.println("Headers: \n" + response.getHeaders());
+
+    }
+
+}
---
+++ src/test/java/com/techproed/day01/GetRequest02.java
@@ -0,0 +1,24 @@
+package com.techproed.day01;
+
+public class GetRequest02 {
+
+
+     /*
+    Bir GET Request asagida verilen Endpoint'e yollanir
+           https://restful-booker.herokuapp.com/booking
+         status code 200'dur
+    a content type  "application/json" dir.
+
+    https://restful-booker.herokuapp.com/booking url'ine
+    accept type'i "application/json" olan GET request'i yolladigimda
+    gelen response'un
+    status kodunun 200
+    content type'inin "application/json" oldugunu test edin
+
+    https://restful-booker.herokuapp.com/booking/1001 url'ine
+    accept type'i "application/json" olan GET request'i yolladigimda
+    gelen response'un
+    status kodunun 404 oldugunu
+   */
+
+}
